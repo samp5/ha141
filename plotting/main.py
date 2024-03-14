240920adbbf52f2831e8841303e445dd4991ca8c
Sam Praneis <sampraneis@gmail.com>
2024-03-14 18:46:02 -0500

    images

--- plotting/main.py
+++ plotting/main.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
 data = []
-with open('../logs/1710456871.log', 'r') as file:
+with open('../logs/1710458559.log', 'r') as file:
     for line in file:
         parts = line.split()
         group_id = int(parts[0])
