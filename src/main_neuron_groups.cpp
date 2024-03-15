41d7f259b6e937800581eccd7d303f6611fafddf
Sam Praneis <sampraneis@gmail.com>
2024-03-15 18:40:24 -0500

    comment main

--- src/main_neuron_groups.cpp
+++ src/main_neuron_groups.cpp
@@ -27,63 +27,63 @@ ostream &STREAM = cout;
 
 Log lg;
 
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t log_mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t activation_mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t message_mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_barrier_t barrier;
+// Protects the membrane_potential
+pthread_mutex_t potential_mutex = PTHREAD_MUTEX_INITIALIZER;
 
+// Protects output stream
+pthread_mutex_t log_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+// Protects activation
+pthread_mutex_t activation_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+// Protects messaging
+pthread_mutex_t message_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+// This is only for non-group runs
 volatile double value = 0;
 
+// This is only for non-group runs
 bool finish = false;
+
+// This is for group runs
 bool active = true;
 
 int main(int argc, char **argv) {
 
+  // If we are unable to parse, just quit
   if (!parse_command_line_args(argv, argc)) {
-    pthread_mutex_destroy(&mutex);
+    pthread_mutex_destroy(&potential_mutex);
     pthread_mutex_destroy(&log_mutex);
     pthread_mutex_destroy(&message_mutex);
     pthread_mutex_destroy(&activation_mutex);
     return 0;
   }
 
+  // Set seed
   srand(RAND_SEED);
 
+  // Reserve the vector memory
   vector<NeuronGroup *> neuron_groups(NUMBER_GROUPS);
 
-  // will potentially leave a remainder
-  int neuron_per_group = NUMBER_NODES / NUMBER_GROUPS;
+  lg.print("\nAdding Neurons\n");
+  lg.print("==================");
 
-  // reamainder is guearenteed to be less than the number of groups
-  int remainder = NUMBER_NODES % NUMBER_GROUPS;
+  // Assign neurons to groups
+  assign_groups(neuron_groups);
 
-  cout << "\nAdding Neurons\n";
-  cout << "=================";
-
-  for (int i = 0; i < NUMBER_GROUPS; i++) {
-
-    // check for remainder and the neurons in the current group accordingly
-    int per_group = remainder ? neuron_per_group + 1 : neuron_per_group;
-
-    if (remainder)
-      remainder--;
-
-    // allocate for this group
-    NeuronGroup *this_group = new NeuronGroup(i + 1, per_group);
-
-    // add to vector
-    neuron_groups.at(i) = this_group;
-  }
-
+  // Add random edges between neurons
   random_group_neighbors(neuron_groups, NUMBER_EDGES);
+
+  // Get message vector from file
   vector<Message *> messages =
       construct_message_vector_from_file(neuron_groups, INPUT_FILE);
 
+  // Print out groups
   for (auto group : neuron_groups) {
     group->print_group();
   }
 
+  // Print out messages
   for (auto message : messages) {
     print_message(message);
   }
@@ -95,6 +95,8 @@ int main(int argc, char **argv) {
   // create messager thread and pass pointer to message array
   pthread_create(&messaging_thread, NULL, send_message_helper,
                  (void *)&messages);
+
+  // create decay thread and pass pointer to neuron groups
   pthread_create(&decay_thread, NULL, decay_helper, (void *)&neuron_groups);
 
   // start all group threads
@@ -102,6 +104,7 @@ int main(int argc, char **argv) {
     group->start_thread();
   }
 
+  // sleep for runtime
   usleep(RUN_TIME);
   active = false;
 
@@ -110,11 +113,13 @@ int main(int argc, char **argv) {
 
   deallocate_message_vector(&messages);
 
+  // Deallocate groups
   for (auto group : neuron_groups) {
     delete group;
   }
 
-  pthread_mutex_destroy(&mutex);
+  // Destroy mutexes
+  pthread_mutex_destroy(&potential_mutex);
   pthread_mutex_destroy(&log_mutex);
   pthread_mutex_destroy(&message_mutex);
   pthread_mutex_destroy(&activation_mutex);
