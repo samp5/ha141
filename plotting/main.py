63ef7c56e2310f6d55490147a3274a2e36893355
Sam Praneis <sampraneis@gmail.com>
2024-03-18 09:57:28 -0500

    Added type information to plot

--- plotting/main.py
+++ plotting/main.py
@@ -1,21 +1,23 @@
 import matplotlib.pyplot as plt
 
 data = []
-with open('../logs/1710548245.log', 'r') as file:
+with open('../logs/1710773400.log', 'r') as file:
     for line in file:
         parts = line.split()
         group_id = int(parts[0])
         neuron_id = int(parts[1])
-        timestamp = float(parts[2])
-        value = float(parts[3])
-        data.append((group_id, neuron_id, timestamp, value))
+        neuron_type = parts[2]
+        timestamp = float(parts[3])
+        value = float(parts[4])
+        data.append((group_id, neuron_id, neuron_type, timestamp, value))
 
 unique_ids = {(entry[0], entry[1]) for entry in data}
 
 for group_id, neuron_id in unique_ids:
 
-    filtered_data = [(entry[2], entry[3]) for entry in data if entry[0] == group_id and entry[1] == neuron_id]
+    filtered_data = [(entry[3], entry[4], entry[2]) for entry in data if entry[0] == group_id and entry[1] == neuron_id]
 
+    neuron_type = filtered_data[0][2]
     sorted_data = sorted(filtered_data, key=lambda x: x[0])
     
     x_values = [entry[0] for entry in sorted_data]
@@ -26,7 +28,7 @@ for group_id, neuron_id in unique_ids:
     plt.plot(x_values, y_values, marker='o' , label=f'Group ID: {group_id}, Item ID: {neuron_id}')
     plt.xlabel('Time')
     plt.ylabel('Membrane Potential')
-    plt.title(f'Group ID: {group_id}, Neuron ID: {neuron_id}')
+    plt.title(f'Group ID: {group_id}, Neuron ID: {neuron_id}, Type: {neuron_type}')
     plt.legend()
     plt.grid(True)
     plt.savefig(f"group_{group_id}neuron_{neuron_id}.png")
