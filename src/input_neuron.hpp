0fbb94927e068d7d70f827b51d0d64064b7cf8fe
Sam Praneis <sampraneis@gmail.com>
2024-04-14 15:06:39 -0500

    input neuron header updates for stimulus switching

--- src/input_neuron.hpp
+++ src/input_neuron.hpp
@@ -3,8 +3,12 @@
 
 #include "neuron.hpp"
 #include "neuron_group.hpp"
+#include <pthread.h>
 
 extern double INPUT_PROB_SUCCESS;
+extern pthread_mutex_t stimulus_switch_mutex;
+extern pthread_cond_t stimulus_switch_cond;
+extern bool switching_stimulus;
 
 class InputNeuron : public Neuron {
 private:
@@ -12,7 +16,10 @@ private:
   double probalility_of_success = INPUT_PROB_SUCCESS;
 
 public:
-  InputNeuron(int _id, NeuronGroup *group);
+  InputNeuron(int _id, NeuronGroup *group) : Neuron(_id, -1, group, Input) {
+    this->activate();
+  }
+
   void run_in_group();
   bool poisson_result();
   void set_input_value(double value);
