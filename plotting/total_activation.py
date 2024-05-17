3081c7bc926591570032e10fb74cd75f40f84659
Sam Praneis <sampraneis@gmail.com>
2024-05-17 09:46:03 -0500

    add options for generating selected graphs from data

--- plotting/total_activation.py
+++ plotting/total_activation.py
@@ -18,18 +18,35 @@ class Options:
             if not self.log_file.exists():
                 print("Error: most recent log file does not exist")
                 quit()
+        else:
+            if args.filename:
+                self.log_title = args.filename
+                if self.log_title[-4:] == ".log":
+                    self.log_title = self.log_title[:-4]
+                self.log_file = Path(f"../logs/{self.log_title}/{self.log_title}.log")
+            else:
+                print("Error: No file supplied")
+                quit()
+            if not self.log_file.exists():
+                print(f"Error: {self.log_file} does not exist")
+                quit()
         self.timestep = args.timestep
         self.graph = args.graph
+        self.graphs = args.graphfor
+        self.csv = args.csv
 
 def parser():
     parser = argparse.ArgumentParser(
             prog="Data parser for SNN",
             description="Create graphs, numpy matrices from log files",)
+    parser.add_argument("-f","--filename", default="", dest="filename", help = "log filename to use", )
     parser.add_argument("-r", "--recent", action='store_true', help ="Use the most recent log file")
+    parser.add_argument("-c", "--csv", action='store_true', help ="output CSV")
     parser.add_argument("-g", "--graph", action='store_true', help="Generate png results")
+    parser.add_argument("-G", "--graphfor", nargs='*', dest="graphfor", default=[], help="Specify the stimulus number(s) for which to generate graphs")
     parser.add_argument("-s", "--step", type=float, dest="timestep", default=-1, help="Specify the timestep to be used in data processing")
 
-    return parser.parse_args()
+    return parser, parser.parse_args()
 
 def get_most_recent_log():
     log_dir = ""
@@ -57,15 +74,22 @@ def generate_graph(stim_num: int,x_values: list[float], y_values: list[float], o
 
 
 def main():
-    args = parser()
+    psr, args = parser()
     opts = Options(args)
 
-    max_stim = 0;
-    min_stim = 0;
+    if not any([opts.graph, opts.graphs != [], opts.csv]):
+        print("-> No output format indicated, not processing file")
+        print()
+        psr.print_help()
+        quit()
+
+    min_stim = 100000000000
+    max_stim = -1
+
     data = {}
+    print("-> Processing file...")
     with open(opts.log_file, 'r') as file:
         for line in file:
-            print(line)
             parts = line.split()
             timestamp = float(parts[3])
             value = float(parts[4])
@@ -82,12 +106,16 @@ def main():
                     data[stimulus_number] = [DataPoint(timestamp, value, stimulus_number)]
 
 # sort the data
+    print("-> Sorting data...")
     for stim_num in data:
         data[stim_num] = sorted(data[stim_num], key = lambda x: x.time)
 
 # all stimuli
+    update_index = (max_stim - min_stim) / 10
     np_matrix = []
-    for stim_num in data:
+    for stim_num in range(min_stim, max_stim + 1):
+        if stim_num % update_index == 0:
+            print(f"-> Processing stimulus {stim_num}...")
         stim_data = data[stim_num]
         # timestep per bin
         opts.timestep = (stim_data[-1].time - stim_data[0].time) / 300 
@@ -108,9 +136,14 @@ def main():
 
         if opts.graph:
             generate_graph(stim_num, x_values, y_values, opts)
+        elif stim_num in opts.graphs:
+            print(f"-> Generating graph for stimulus {stim_num}...")
+            generate_graph(stim_num, x_values, y_values, opts)
 
-    a = np.matrix(np_matrix)
-    np.savetxt(f"{opts.log_title}.csv", a, delimiter = ",", fmt='%d')
+    if opts.csv:
+        print(f"-> Generating CSV...")
+        a = np.matrix(np_matrix)
+        np.savetxt(f"{opts.log_title}.csv", a, delimiter = ",", fmt='%d')
 
 if __name__ == "__main__":
     main()
