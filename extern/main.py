34dc0617d5843e3ff9ce8f1bdf2a62d8df93c2a7
Sam Praneis <sampraneis@gmail.com>
2024-05-24 10:56:34 -0500

    Change csv naming

--- extern/main.py
+++ extern/main.py
@@ -1,4 +1,5 @@
 import snn
+from datetime import datetime
 import numpy as np
 import pandas as pd
 from sklearn.datasets import fetch_openml
@@ -6,9 +7,12 @@ from sklearn.datasets import fetch_openml
 def div(x: int) -> float:
     return float(x) / 255.0
 # Load the MNIST dataset
-mnist = fetch_openml('mnist_784', version=1)
+dataset = "mnist_784"
+v = 1
+mnist = fetch_openml(dataset, version=v)
 images, labels = mnist["data"], mnist["target"] # type: ignore
 images = images.map(div).to_numpy()
+print(f"shape of images is: {images.shape}")
 
 list = ["dummy", "base_config.toml"]
 net = snn.pySNN(list)
@@ -16,4 +20,5 @@ net.generateSynapses()
 net.start(images)
 net.join()
 out = net.getActivation()
-np.savetxt("data.csv", out, fmt = '%d')
+filestr = datetime.now().strftime("%m%d_%H%M")
+np.savetxt(f"{dataset}v{v}_{filestr}.csv", out,delimiter=",", fmt = '%d')
