ee41808942a770243773ff983338042dd2cc423b
Sam Praneis <sampraneis@gmail.com>
2024-07-06 16:44:08 -0500

    MINOR ignore adj list files

--- networkX/networkGen.py
+++ networkX/networkGen.py
@@ -2,11 +2,11 @@ import networkx as nx;
 
 def main():
     G = nx.navigable_small_world_graph(4)
-    nx.write_adjlist(G, "out")
+    nx.write_adjlist(G, "out.adj")
     print(G);
 
     G2 = nx.turan_graph(4, 2);
-    nx.write_adjlist(G2, "out2")
+    nx.write_adjlist(G2, "out2.adj")
     print(G2);
  
 if __name__ == "__main__":
