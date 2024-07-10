c72253f8d0be6eca1be1715aadef50dabe7096bc
Sam Praneis <sampraneis@gmail.com>
2024-07-10 15:40:00 -0500

    ADD test code to networkGen.py

--- networkX/networkGen.py
+++ networkX/networkGen.py
@@ -1,13 +1,33 @@
-import networkx as nx;
+import networkx as nx
+import random
 
 def main():
-    G = nx.navigable_small_world_graph(84)
+    G = nx.navigable_small_world_graph(10)
     nx.write_adjlist(G, "out.adj")
     print(G);
 
     G2 = nx.turan_graph(4, 2);
     nx.write_adjlist(G2, "out2.adj")
     print(G2);
+
+
+    edgeWeights = {}
+    for n in G:
+        for nbr in G[n]:
+            G[n][nbr]["weight"] = random.random()
+            edgeDict = {}
+            edgeDict["weight"] = G[n][nbr]["weight"]
+            edgeWeights[n, nbr] = edgeDict
+
+
+    for key, val in edgeWeights.items():
+        edgeWeights[key]["weight"] = val["weight"]+ 10;
+
+    nx.write_weighted_edgelist(G, "weight.adj")
+    nx.set_edge_attributes(G, edgeWeights);
+
+
+    print(nx.get_edge_attributes(G, "weight"))
  
 if __name__ == "__main__":
     main()
