d8fdc169f75a11d265b3f094b322c30ddc0b1ac9
Sam Praneis <sampraneis@gmail.com>
2024-10-24 11:28:13 -0500

    Add comments for FunctionIdentifier

--- src/funciton_id.h
+++ src/funciton_id.h
@@ -1,13 +1,36 @@
 #ifndef FUNCTION_IDENTIFIER
 #define FUNCTION_IDENTIFIER
 #define MIN_FUNC_ID 0
-#define MAX_FUNC_ID 1
+#define MAX_FUNC_ID 4
 
 enum FunctionIdentifier {
+  /*
+   * Analagous to pySNN::initiaize(AdjDict dict, NUMBER_INPUT_NEURONS)
+   */
   Initiaize,
+  /*
+   * Analagous to pySNN::pySNN(ConfigDict)
+   */
   Construct,
+  /*
+   * pySNN::runBatch(ConfigDict)
+   */
+  RunBatch,
+  /*
+   * pySNN::getActivation(ConfigDict)
+   */
+  GetActivation,
+  /*
+   * pySNN::batchReset(ConfigDict)
+   */
+  BatchReset,
   UNKNOWN_FUNC
 }; // if you change this change the MAX_FUNC_ID and MIN_FUNC_ID accordingly
    // please it will not work if you do not
+
+enum Response {
+  Sucesss,
+  Fail,
+};
 
 #endif // !FUNCTION_IDENTIFIER
